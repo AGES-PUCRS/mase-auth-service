8c3ceb6c8bb88c79a98bf934c695bcb7c0363a5e
Felipe Guedes <guedes4g@hotmail.com>
2019-06-01 08:32:50 -0300

    Removendo Lombok

--- src/main/java/br/pucrs/ages/mase/authservice/dto/RefreshRequestDto.java
+++ src/main/java/br/pucrs/ages/mase/authservice/dto/RefreshRequestDto.java
@@ -1,13 +1,22 @@
 
 package br.pucrs.ages.mase.authservice.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-
-@Data @NoArgsConstructor @AllArgsConstructor @ToString
 public class RefreshRequestDto {
-	private String refreshToken;	
-}
+	private String refreshToken;
 
+	public RefreshRequestDto() {
+	}
+
+	public RefreshRequestDto(String refreshToken) {
+		this.refreshToken = refreshToken;
+	}
+
+	public String getRefreshToken() {
+		return refreshToken;
+	}
+
+	public void setRefreshToken(String refreshToken) {
+		this.refreshToken = refreshToken;
+	}
+
+}
